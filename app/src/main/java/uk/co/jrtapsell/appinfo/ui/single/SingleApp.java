ebb5fbc21517f8ca6fb72168218a65db649a86e3
James Tapsell <email@jrtapsell.co.uk>
2017-06-19 15:19:35 +0100

    Simplified app code

--- app/src/main/java/uk/co/jrtapsell/appinfo/ui/single/SingleApp.java
+++ app/src/main/java/uk/co/jrtapsell/appinfo/ui/single/SingleApp.java
@@ -2,6 +2,7 @@ package uk.co.jrtapsell.appinfo.ui.single;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,35 +21,42 @@ import uk.co.jrtapsell.appinfo.data.permission.MyPermission;
 
 public class SingleApp extends AppCompatActivity {
 
+    private AppFactory appFactory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final AppFactory appFactory = AppFactory.getInstance(getPackageManager());
+        appFactory = AppFactory.getInstance(getPackageManager());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_app);
-        Bundle intentExtras = getIntent().getExtras();
-        String appName = intentExtras.getString("app");
-        if (appName == null) {
-            throw new AssertionError("Empty app name");
-        }
-        final MyApp app = appFactory.getApp(appName);
+
         ListView listView = (ListView) findViewById(R.id.permissionsId);
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress);
+
+        final MyApp app = getApp();
+        
         List<MyPermission> perms = app.getPermissions();
         Collections.sort(perms);
 
         listView.setAdapter(new PermissionAdapter(this, perms));
 
-        ActionBar topBar = getSupportActionBar();
+        setupTopBar(app);
 
-        if (topBar == null) {
-            throw new AssertionError("getSupportActionBar() returned null");
+        setupButtons(app);
+        progressBar.setVisibility(View.INVISIBLE);
+        listView.setVisibility(View.VISIBLE);
+    }
+
+    @NonNull
+    private MyApp getApp() {
+        Bundle intentExtras = getIntent().getExtras();
+        String appName = intentExtras.getString("app");
+        if (appName == null) {
+            throw new AssertionError("Empty app name");
         }
+        return appFactory.getApp(appName);
+    }
 
-        topBar.setDisplayUseLogoEnabled(true);
-        topBar.setDisplayShowHomeEnabled(true);
-        topBar.setTitle(app.getName());
-        topBar.setIcon(app.getIcon());
-
+    private void setupButtons(MyApp app) {
         final Intent manageIntent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         manageIntent.addCategory(Intent.CATEGORY_DEFAULT);
         manageIntent.setData(Uri.parse("package:" + app.getPackageName()));
@@ -76,7 +84,18 @@ public class SingleApp extends AppCompatActivity {
                 startActivity(openIntent);
             }
         });
-        progressBar.setVisibility(View.INVISIBLE);
-        listView.setVisibility(View.VISIBLE);
+    }
+
+    private void setupTopBar(MyApp app) {
+        ActionBar topBar = getSupportActionBar();
+
+        if (topBar == null) {
+            throw new AssertionError("getSupportActionBar() returned null");
+        }
+
+        topBar.setDisplayUseLogoEnabled(true);
+        topBar.setDisplayShowHomeEnabled(true);
+        topBar.setTitle(app.getName());
+        topBar.setIcon(app.getIcon());
     }
 }
